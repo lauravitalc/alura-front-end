3bb596866191e9d4841dd2e65cbc74c3a530b846
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-05-03 19:00:03 -0300

    Modo noturno

---
+++ javascript/modo_noturno/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector("#btn").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode")
+})
+
+/* Toggle -> Alternancia  
+
+Ao clicar no botão #btn, mude o body para a classe dark-mode */
+
