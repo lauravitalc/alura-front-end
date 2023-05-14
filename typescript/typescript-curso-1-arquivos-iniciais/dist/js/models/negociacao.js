f83fa1e8bdea7a882b74ee400cc71e8eadaf7ca2
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-05-14 19:44:04 -0300

    Javascript e Typescript (Incompleto)

---
+++ typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -0,0 +1,11 @@
+class Negociacao {
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+}
