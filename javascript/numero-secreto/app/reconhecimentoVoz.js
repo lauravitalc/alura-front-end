a3e9801eeee0a02d234e8bef462f24adfb40a82a
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-05-18 21:44:54 -0300

    Reconhecimento de Voz - 4

---
+++ javascript/numero-secreto/app/reconhecimentoVoz.js
@@ -0,0 +1,22 @@
+const elementoChute = document.getElementById('chute');
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start();
+
+// Quando nossa recognition começar, quero que pegue o result e ....
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript;
+    exibeChute(chute);
+}
+
+function exibeChute(chute){
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
