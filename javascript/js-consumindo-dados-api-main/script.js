4e19df73efa0c7c1d9b1f84f2989b5b49ad26db7
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-05-26 21:51:51 -0300

    JavaScript: consumindo e tratando dados de uma API - 4

--- javascript/js-consumindo-dados-api-main/script.js
+++ javascript/js-consumindo-dados-api-main/script.js
@@ -1,15 +1,23 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/").then(resposta => resposta.json())
-                                                                 .then(r => {
-                                                                            if(r.erro){
-                                                                                throw Error("Esse CEP não existe.")
-                                                                            } else {
-                                                                            console.log(r)}
-                                                                        })
-                                                                 .catch(erro => console.log(erro))
-                                                                 .finally(msg => console.log('Processamento concluído'));
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP =  await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if(consultaCEPConvertida.erro){
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida;
+    } catch (erro){
+        console.log(erro)
+    }
+}
+
 // O corpo da resposta de uma requisição chega em formato de bytes. 
 // Desta forma o .json() transforma o corpo e retorna um json formatado. 
 // O formato JSON (JavaScript Object Notation) possui basicamente a mesma sintaxe que a de um objeto JS.
 
-console.log(consultaCEP)
 
+
+// let ceps = ['01001000', '31110120', '01001001'];
+// let conjuntoCeps = ceps.map(valor => buscaEndereco(valor));
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
