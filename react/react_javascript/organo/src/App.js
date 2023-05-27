146e966fa7bc4c88d4274e3de5e76dee4f58e2ab
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-05-27 14:04:26 -0300

    React: desenvolvendo com JavaScript - 1

---
+++ react/react_javascript/organo/src/App.js
@@ -0,0 +1,11 @@
+import Banner from './componentes/Banner/banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react_javascript/organo/src/componentes/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner() {
+    // JSX 
+    return (
+        <header className="banner">
+             <img src="/imagens/banner.png" alt="O Banner principal da página" />
+        </header>
+    )
+}
+
+export default Banner
