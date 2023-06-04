9c63cdcb022dcafac079fc63f402f003f3f58835
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-06-04 18:58:03 -0300

    React: desenvolvendo com JavaScript - 2

--- react/react_javascript/organo/src/componentes/Formulario/index.js
+++ react/react_javascript/organo/src/componentes/Formulario/index.js
@@ -1,14 +1,32 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Sciente',
+        'DevOps',
+        'UX e Design',
+        'Mobile']
+    
+    const aoSalvar = (e) => {
+        e.preventDefault()
+        console.log('OI')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar }>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
