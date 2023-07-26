bb80f3a5ba114fe6b873985bbe3a05eb8c80dbab
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-07-26 18:42:23 -0300

    POO - Part 1

---
+++ javascript/projeto_banco_poo/index.js
@@ -0,0 +1,20 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = 'Laura';
+cliente1.cpf = 11122233345;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
+cliente2.nome = 'Gabriel';
+cliente2.cpf = 22233344456;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente2);
