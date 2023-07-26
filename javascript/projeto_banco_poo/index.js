b9a1b4af1ffbb06ca5f84de5419c46e0c65d9aa3
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-07-26 19:48:09 -0300

    POO - Part 2

--- javascript/projeto_banco_poo/index.js
+++ javascript/projeto_banco_poo/index.js
@@ -1,20 +1,48 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 }
+
+class ContaCorrente{
+    agencia;
+    // #saldo = 0;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+           return;
+        }
+        
+        this._saldo += valor;
+        return valor;
+    }
+
+}
+
 
 const cliente1 = new Cliente();
 cliente1.nome = 'Laura';
 cliente1.cpf = 11122233345;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = 'Gabriel';
 cliente2.cpf = 22233344456;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente2);
+const contaCorrente1 = new ContaCorrente();
+contaCorrente1.agencia = 1001;
+console.log(contaCorrente1);
+
+contaCorrente1.depositar(150);
+console.log(contaCorrente1);
+
+const valorSacado = contaCorrente1.sacar(50);
+console.log(valorSacado);
+console.log(contaCorrente1);
+
