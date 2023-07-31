a09c53ec25187765eed9e45c8fa690ed3a629d65
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-07-31 11:17:16 -0300

    OO - Part 3

---
+++ javascript/projeto_banco_poo/ContaCorrente.js
@@ -0,0 +1,28 @@
+export class ContaCorrente{
+    agencia;
+    cliente;
+    // #saldo = 0;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+           return;
+        }
+        
+        this._saldo += valor;
+        return valor;
+    }
+
+    tranferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+
+}
--- javascript/projeto_banco_poo/index.js
+++ javascript/projeto_banco_poo/index.js
@@ -1,31 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    // #saldo = 0;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-           return;
-        }
-        
-        this._saldo += valor;
-        return valor;
-    }
-
-}
-
+import { Cliente } from "./cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = 'Laura';
@@ -37,12 +11,19 @@ cliente2.cpf = 22233344456;
 
 const contaCorrente1 = new ContaCorrente();
 contaCorrente1.agencia = 1001;
-console.log(contaCorrente1);
+contaCorrente1.cliente = cliente1;
 
-contaCorrente1.depositar(150);
-console.log(contaCorrente1);
+const contaCorrente2 = new ContaCorrente();
+contaCorrente2.cliente = cliente2;
+contaCorrente2.agencia = 1002;
 
-const valorSacado = contaCorrente1.sacar(50);
-console.log(valorSacado);
 console.log(contaCorrente1);
+console.log(contaCorrente2);
+
+let valorTransferencia = 200;
+contaCorrente1.depositar(500);
+contaCorrente1.tranferir(valorTransferencia, contaCorrente2);
+
+console.log(contaCorrente1);
+console.log(contaCorrente2);
 
