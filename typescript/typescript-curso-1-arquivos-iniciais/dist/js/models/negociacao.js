bef39c93f4acbe431d70bca28255043b577b2d21
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-08-17 19:40:57 -0300

    Typescript I - Part 1

--- typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -1,4 +1,4 @@
-class Negociacao {
+export class Negociacao {
     #data;
     #quantidade;
     #valor;
@@ -7,5 +7,21 @@ class Negociacao {
         this.#data = data;
         this.#quantidade = quantidade;
         this.#valor = valor;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade(){
+        return this.#quantidade;
+    }
+
+    get valor(){
+        return this.#valor;
+    }
+
+    get volume(){
+        return this.#quantidade * this.#valor;
     }
 }
