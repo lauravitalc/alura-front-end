64e13934c9dbe7948285fbdffc67da40e3e8d7de
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-08-27 15:22:42 -0300

    Typescript I - part 3

---
+++ typescript/typescript-curso-1-arquivos-iniciais/dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,27 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        // Tratamento
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputData.value);
+        const valor = parseFloat(this.inputData.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
