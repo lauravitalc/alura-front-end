8e8d90ec5c733966e9cb99af228c0ef167315794
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-08-28 16:32:56 -0300

    Typescript I - Part 5

--- typescript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,27 +1,21 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(
+        private _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ) {}
+
+    get volume(): number{
+        return this.quantidade * this.valor;
     }
 
     get data(): Date {
+        // Programação Defensiva
+        const data = new Date(this._data.getTime());
         return this._data;
-    }
-
-    get quantidade(): number{
-        return this._quantidade;
-    }
-
-    get valor(): number{
-        return this._valor;
-    }
-
-    get volume(): number{
-        return this._quantidade * this._valor;
     }
 }
